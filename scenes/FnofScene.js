b292d05d1d53853435e10909eef0579b33cbc422
levlups <nabilassine@gmail.com>
2024-05-15 23:12:04 -0400

    main

--- scenes/FnofScene.js
+++ scenes/FnofScene.js
@@ -7,17 +7,47 @@ export default class Fnofcene extends Phaser.Scene {
 
     preload(){
 		this.load.image("pepe", "assets/menu.png");
+		this.load.image("flash", "assets/flashlight.png");
+		  //  this.load.image('scare', 'assets/scare.gif');
+			 this.load.spritesheet('scare', 'assets/scare.png', { frameWidth: 500, frameHeight: 500 });
+			  this.load.spritesheet('screen', 'assets/screen.png', { frameWidth: 500, frameHeight: 500 });
 		this.load.spritesheet("buttons", "UIpack_vector.svg",{ frameWidth: 230, frameHeight: 65 });
 	}
     create() {
 
 	     // Trigger the flickering effect
        // this.flickerCamera(5000, 0.5, 1, 50); // Flicker for 5 seconds
+   this.anims.create({
+                key: 'playGif',
+                frames: this.anims.generateFrameNumbers('scare', { start: 0, end: 12 }),
+                frameRate: 20,
+                repeat: false
+            });
+			
+			
+			this.anims.create({
+                key: 'playScreen',
+                frames: this.anims.generateFrameNumbers('screen', { start: 0, end: 4 }),
+                frameRate: 10,
+                repeat: -1
+            });
 
 
 		//this.load.image("pepe", "pepe.png");
 		//this.add.sprite(0, 0, 'pepe');
 		const poster=this.add.sprite(400,400,"pepe").setOrigin(0.5)
+		
+		const flashy=this.add.sprite(400,400,"flash").setOrigin(0.5)
+		
+		this.time.delayedCall(5000, function() {
+                 var gif = this.add.sprite(400, 300, 'scare');
+                gif.play('playGif');
+            }, [], this);
+			
+			this.time.delayedCall(7000, function() {
+                 var scr = this.add.sprite(400, 300, 'screen');
+                scr.play('playScreen');
+            }, [], this);
 		
 		//poster.displayHeight = 600;
    // poster.displayWidth = 800;
